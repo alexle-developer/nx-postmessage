cc4c99b620b5e99dec4ee11fd0fe33b3e5622f0b
Alex Le <ale@americanfirstfinance.com>
2024-02-27 11:17:24 -0600

    commit parent app

--- apps/parent-app/src/app/app.component.ts
+++ apps/parent-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
@@ -9,6 +9,20 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'parent-app';
+  message = '';
+  ngOnInit(): void {
+    this.message = '';
+  }
+
+  // write a send message method that capture the value from the textarea and console log it
+  sendMessage = (message: string) => {
+    console.log('message sent:', message);
+  };
+
+  onMessage = (event: MessageEvent) => {
+    console.log('message received', event.data);
+    this.message = event.data;
+  };
 }
